e8ff9bdeb718d265567cf9a3cac8d3274ac70b6c
siphonhata <siphonhata@gmail.com>
2024-11-19 15:23:28 +0200

    product-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { Product } from './product';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [products, setProducts] = useState<Product []>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts(prevState => [...prevState, {
+      id: prevState.length + 101,
+      name: 'product' + (prevState.length +1),
+      price: (prevState.length *100) +100,
+      brand: "Some Bramd",
+      description: "Descr",
+      pictureUrl: "url"
+    }])
+      
+      
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}> {product.name} - R{product.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </div>
   )
 }
 
