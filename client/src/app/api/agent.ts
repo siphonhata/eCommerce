d1c20f550f1b4e45154a759d3801bf1009b08f7b
siphonhata <siphonhata@gmail.com>
2024-11-27 15:27:12 +0200

    Delay and Loading functionality

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 
-const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
+const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
